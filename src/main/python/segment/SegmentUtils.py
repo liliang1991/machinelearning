6275fbd90b8f4293cbc34723cd31975f91e12efd
liliang <liliang1@ultrapower.com.cn>
2018-06-04 13:48:44 +0800

    增加结巴分词工具类

---
+++ src/main/python/segment/SegmentUtils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import jieba
+def segment(value,type):
+    seg_list = jieba.cut(value, cut_all=type)
+    print("Full Mode: " + "/ ".join(seg_list))
+
+
+
+if __name__ == '__main__':
+    print(pd.__version__)
+    segment("我来到北京清华大学",False)
