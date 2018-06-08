8cadcb6708fd933a64be4cd7b0d427da2a04b3d6
liliang <liliang1@ultrapower.com.cn>
2018-06-08 12:34:04 +0800

    countvectorizer 测试类

--- src/main/python/mlib/countvectorizer/test/count_vectorizer_test.py
+++ src/main/python/mlib/countvectorizer/test/count_vectorizer_test.py
@@ -1,13 +1,12 @@
 from pyspark.ml.feature import CountVectorizer
 from pyspark.shell import spark
+from mlib.countvectorizer.util.countvectorizer_utils import *
 
 df = spark.createDataFrame([
     (0, "a b c".split(" ")),
     (1, "a b b c a".split(" "))
 ], ["id", "words"])
-#mindf 必须在文档中出现的最少次数
+# mindf 必须在文档中出现的最少次数
 # vocabSize 词典大小
-cv = CountVectorizer(inputCol="words", outputCol="features", vocabSize=3, minDF=2.0)
-model = cv.fit(df)
-result = model.transform(df)
+result = CountVectorizerModel("words", "features", 3, 2.0, df)
 result.show(truncate=False)
