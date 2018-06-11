489d46d5e61865a5708ffb16114d62dc52ce6b79
liliang <liliang1@ultrapower.com.cn>
2018-06-11 17:59:53 +0800

    test 类

--- src/main/python/mlib/countvectorizer/test/count_vectorizer_test.py
+++ src/main/python/mlib/countvectorizer/test/count_vectorizer_test.py
@@ -1,5 +1,6 @@
 from pyspark.ml.feature import CountVectorizer
 from pyspark.shell import spark
+from pyspark.ml.feature import StandardScaler
 from mlib.countvectorizer.util.countvectorizer_utils import *
 
 df = spark.createDataFrame([
@@ -9,4 +10,13 @@ df = spark.createDataFrame([
 # mindf 必须在文档中出现的最少次数
 # vocabSize 词典大小
 result = CountVectorizerModel("words", "features", 3, 2.0, df)
-result.show(truncate=False)
+
+scaler = StandardScaler(inputCol="features", outputCol="scaledFeatures",
+                        withStd=True, withMean=False)
+
+# Compute summary statistics by fitting the StandardScaler
+scalerModel = scaler.fit(result)
+
+# Normalize each feature to have unit standard deviation.
+scaledResult = scalerModel.transform(result)
+scaledResult.show(truncate=False)
