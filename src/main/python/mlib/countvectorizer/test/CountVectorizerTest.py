01a2175017ef10a16be61910a12a42fff5a557a2
liliang <liliang1@ultrapower.com.cn>
2018-06-21 19:02:08 +0800

    first commit

--- src/main/python/mlib/countvectorizer/test/CountVectorizerTest.py
+++ src/main/python/mlib/countvectorizer/test/CountVectorizerTest.py
@@ -1,9 +1,13 @@
-from mlib.countvectorizer.util.countvectorizer_utils import *
+from mlib.countvectorizer.util.CountVectorizerUtils import *
 from mlib.standard.util.StandardScalerUtils import *
+from mlib.stringindexer.util.StringIndexerUtils import *
 df = spark.createDataFrame([
-    (0, "a b c".split(" ")),
-    (1, "a b b c a".split(" "))
-], ["id", "words"])
+    ("交通", "专家给武汉交通支招：建设快速公交通"),
+    ("交通", "随着北方气温的迅速回升，大量省内"),
+    ("交通", "武汉铁路东线将提速改造 浏览次数")
+], ["label", "text"])
+sindex=StringIndexerModel("words","sindexout",df)
+sindex.show()
 # mindf 必须在文档中出现的最少次数
 # vocabSize 词典大小
 cvResult = CountVectorizerModel("words", "features", 3, 2.0, df)
