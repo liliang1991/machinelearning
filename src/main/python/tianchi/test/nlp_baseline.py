6e2670456f3c0c772469b3adbf5edc6b25aee57c
liliang1991 <liliang_68@126.com>
2018-07-01 21:48:23 +0800

    all

--- src/main/python/tianchi/test/nlp_baseline.py
+++ src/main/python/tianchi/test/nlp_baseline.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import StratifiedKFold
 import lightgbm as lgb
 from sklearn.metrics import log_loss
 import math
-import networkx as nx
 
 def lgb_model(X,y,test):
 	N = 2
@@ -16,11 +15,13 @@ def lgb_model(X,y,test):
 
     # specify your configurations as a dict
 	params = {
+		'max_bin':1000,
+		'num_iterations': 1000,
         'boosting_type': 'gbdt',
         'objective': 'binary',
         'metric': 'binary_logloss',
-        'num_leaves': 60,
-        'learning_rate': 0.05,
+        'num_leaves': 600,
+        'learning_rate': 0.01,
         'feature_fraction': 0.8,
         'bagging_fraction': 0.8,
         'bagging_freq': 5,
@@ -45,31 +46,46 @@ def lgb_model(X,y,test):
 		y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
 		xx_cv.append(log_loss(y_test,y_pred))
 		xx_pre.append(gbm.predict(test, num_iteration=gbm.best_iteration))
+		print(xx_cv)
+		print(xx_pre)
+		print("===========")
 	return xx_cv, xx_pre
-
+#加载数据
+# 英语问句对  英语问句1，西班牙语翻译1，英语问句2，西班牙语翻译2，匹配标注。
 english_spa = pd.read_csv('/home/moon/work/tianchi/data/cikm_english_train_20180516.txt', sep = '\t', header = None)
+#西班牙问句对 西班牙语问句1，英语翻译1，西班牙语问句2，英语翻译2，匹配标注
 spa = pd.read_csv('/home/moon/work/tianchi/data/cikm_spanish_train_20180516.txt', sep = '\t', header = None)
+#需要预测的西班牙语问句对
 test = pd.read_csv('/home/moon/work/tianchi/data/cikm_test_a_20180516.txt', sep = '\t', header = None)
+#测试数据列
 test.columns = ['spa_qura1', 'spa_qura2']
+#英文问句列
 english_spa.columns = ['eng_qura1', 'spa_qura1', 'eng_qura2', 'spa_qura2', 'label']
+#西班牙问句列
 spa.columns = ['spa_qura1', 'eng_qura1', 'spa_qura2', 'eng_qura2', 'label']
+#新增一个label 列
 test['label'] = -1
-
+#提取英语问句对 列    英语问句1  英语问句2 label
 english = english_spa[['eng_qura1','eng_qura2','label']]
+#获取新列名
 english.columns = ['spa_qura1','spa_qura2','label']
-
+#提取西班牙问句对 英语翻译1 英语翻译2
 english1 = spa[['eng_qura1','eng_qura2','label']]
+#获取新列名
 english1.columns = ['spa_qura1','spa_qura2','label']
 pd.set_option('display.max_rows',None)
+# test  英语问句对  西班牙问句对 英语问句对(label==1)
 data = pd.concat([test, english_spa[test.columns], spa[test.columns],english[english.label == 1][test.columns]]).reset_index()
+print("data======")
 
-
+#西班牙语问句1
 data['spa_qura_list_1'] = data['spa_qura1'].apply(lambda x : x.split(' '))
+#西班牙语问句2
 data['spa_qura_list_2'] = data['spa_qura2'].apply(lambda x : x.split(' '))
-
+#西班牙语问句拼到一个list
 spa_list = list(data['spa_qura_list_1'])
 spa_list.extend(list(data['spa_qura_list_2']))
-
+#班牙语问句 Word2Vec
 model = Word2Vec(spa_list, sg=1, size=30,  window=5,  min_count=1,  negative=3, sample=0.001, hs=1, workers=4)
 
 def seq_to_w2v(seq, model):
@@ -87,15 +103,15 @@ data['spa_list_2'] = data['spa_qura_list_2'].apply(lambda x : seq_to_w2v(x, mode
 data[data.label == -1].to_csv('test.csv',index = None)
 data[data.label == 1].to_csv('train.csv', index = None)
 
-
-def calEuclideanDistance(vec1,vec2):  
-    dist = np.sqrt(np.sum(np.square(vec1 - vec2)))  
-    return dist 
-
+#欧式
+def calEuclideanDistance(vec1,vec2):
+    dist = np.sqrt(np.sum(np.square(vec1 - vec2)))
+    return dist
+#曼哈顿
 def Manhattan(vec1, vec2):# Manhattan_Distance,曼哈顿距离
     npvec1, npvec2 = np.array(vec1), np.array(vec2)
     return np.abs(npvec1-npvec2).sum()
-
+#余弦
 def Cosine(vec1, vec2):
     npvec1, npvec2 = np.array(vec1), np.array(vec2)
     return npvec1.dot(npvec2)/(math.sqrt((npvec1**2).sum()) * math.sqrt((npvec2**2).sum()))
@@ -156,16 +172,17 @@ test = data[data['label']==-1]
 
 y = train.pop('label').values
 X = train[feature].values
-
+print(">>>>>>>>>>>>>")
+print(X)
 test_y = test.pop('label').values
 test1 = test[feature].values
-
 result, tt = lgb_model(X, y, test1)
-print(len(tt))
 s = 0
 for i in tt:
         #print (i)
+	print(str(i)+"=======")
 	s = s + i
+	print(s)
 s = s /5
 
 test['label'] = list(s)
